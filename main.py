67ef41de642ce5fb4aaa24c9119b603d9834c9f8
colinholbrook <yourself@stanford.edu>
2022-01-20 13:07:42 -0800

    Remove z from cached filename

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def main(indir, region_index=None, increase_factor=None, growth_plane=None, grow
     nc = image.shape[2] if cf.N_DIMS > 2 else 1
     
     rundir = os.path.basename(os.path.normpath(indir))
-    data_cache_file = f'X:/temp/CellSeg cache/{rundir}_{filename}_z3_{h}x{w}_n{nuclear_index}_if{cf.INCREASE_FACTOR:.1f}_ma{cf.MIN_AREA}_gm{cf.GROWTH_PIXELS_MASKS}.npz'
+    data_cache_file = f'X:/temp/CellSeg cache/{rundir}_{filename}_{h}x{w}_n{nuclear_index}_if{cf.INCREASE_FACTOR:.1f}_ma{cf.MIN_AREA}_gm{cf.GROWTH_PIXELS_MASKS}.npz'
     #data_cache_file = None
     
     if data_cache_file and os.path.exists(data_cache_file):
