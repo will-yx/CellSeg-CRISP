0e639e44577a9f67186bfa447a98eae633a909b7
colinholbrook <yourself@stanford.edu>
2022-01-20 10:36:13 -0800

    Enable caching computed masks

--- main.py
+++ main.py
@@ -115,14 +115,14 @@ def main(indir, region_index=None, increase_factor=None, growth_plane=None, grow
     nc = image.shape[2] if cf.N_DIMS > 2 else 1
     
     rundir = os.path.basename(os.path.normpath(indir))
-    data_cache_file = 'X:/temp/CellVision cache/{}_{}_{}x{}_n{}_if{:.1f}_ma{}_gm{}.npz'.format(rundir,filename,h,w,nuclear_index,cf.INCREASE_FACTOR,cf.MIN_AREA,cf.GROWTH_PIXELS_MASKS)
-    data_cache_file = None
+    data_cache_file = f'X:/temp/CellSeg cache/{rundir}_{filename}_z3_{h}x{w}_n{nuclear_index}_if{cf.INCREASE_FACTOR:.1f}_ma{cf.MIN_AREA}_gm{cf.GROWTH_PIXELS_MASKS}.npz'
+    #data_cache_file = None
     
     if data_cache_file and os.path.exists(data_cache_file):
       with np.load(data_cache_file) as data_cached:
         mask = data_cached['data']
     else:
-      print('\nSegmenting with CellVision:', filename); t0=timer()
+      print('\nSegmenting with CellSeg-CRISP:', filename); t0=timer()
       
       from numba import cuda 
       cuda.get_current_device().reset()
