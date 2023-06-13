722002d1716c1a4ec8411e16ff17c0c778caa122
colinholbrook <yourself@stanford.edu>
2023-06-13 13:29:21 -0700

    convert images to float even if they aren't dynamic-range compressed images
    We need images in contiguous float format for quantification

--- main.py
+++ main.py
@@ -230,6 +230,8 @@ def main(indir, region_index=None, increase_factor=None, growth_plane=None, grow
       if drc0 and drc1 and drca:
         image = cvutils.drcu(image, drc0, drc1, drca)
         print(f'Uncompress dynamic range image stack: {timer()-t0:.1f}s'); t0=timer()
+      else:
+        image = np.ascontiguousarray(image.astype(np.float32))
       
       centroids = stitched_mask.centroids
       xys = np.fliplr(np.around(centroids))
