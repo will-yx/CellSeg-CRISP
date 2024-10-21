da1fbc409503e622770028f9c5ec4b0c822e5848
will-yx <57642509+will-yx@users.noreply.github.com>
2024-10-20 22:15:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def CSquant(mask_dir, indir, region_index=None, growth_plane=None, growth_quant_
         
         print('Save measurements to csv and fcs: {:.1f}s'.format(timer()-t0)); t0=timer()
     
-    if os.isfile("./CellSeg cache/img.arr"):
+    if os.path.isfile("./CellSeg cache/img.arr"):
         os.remove("./CellSeg cache/img.arr")
     
     print('Total processing time for file {}: {:.1f}m'.format(filename, (timer()-t0_file) / 60));
